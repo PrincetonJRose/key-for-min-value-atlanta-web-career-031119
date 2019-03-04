35c40a6a765011ce11c360a00f4c855a9f6bd06e
Princeton Rose <decoyzpr@gmail.com>
2019-03-04 22:53:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,19 @@ def key_for_min_value(name_hash)
   if name_hash.size == 0
     return nil
   end
-  
+  low_key = []
+  low_value = []
+  name_hash.each do |name, number|
+    low_key.push(name)
+    low_value.push(number)
+  end
+  low = low_value[0]
+  for val in 0...low_value
+    if low_value[val] < low
+      low = low_value[val]
+      lowest = low_key[val]
+    end
+  end
+  return lowest
+      
 end
