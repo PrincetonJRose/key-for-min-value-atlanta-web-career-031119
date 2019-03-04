487a84d1c984444364f714a4a7d7cf995d95585e
Princeton Rose <decoyzpr@gmail.com>
2019-03-04 22:55:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,17 +5,17 @@ def key_for_min_value(name_hash)
   if name_hash.size == 0
     return nil
   end
-  low_key = []
-  low_value = []
+  low_k = []
+  low_v = []
   name_hash.each do |name, number|
-    low_key.push(name)
-    low_value.push(number)
+    low_k.push(name)
+    low_v.push(number)
   end
-  low = low_value[0]
-  for val in 0...low_value
-    if low_value[val] < low
-      low = low_value[val]
-      lowest = low_key[val]
+  low = low_v[0]
+  for v in 0...low_v
+    if low_v[v] < low
+      low = low_v[v]
+      lowest = low_k[v]
     end
   end
   return lowest
